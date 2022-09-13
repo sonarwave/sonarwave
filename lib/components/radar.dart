df263a766374cb792e99ea2cdea7cfbab297a189
WaadSulaiman <waad.sulaiman@outlook.com>
2022-09-13 21:37:01 +0200

    Added a radar background theme.

---
+++ lib/components/radar.dart
@@ -0,0 +1,82 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:sonarwave/painters/radar_background_painter.dart';
+import 'package:sonarwave/painters/radar_line_painter.dart';
+
+class Radar extends StatefulWidget {
+  final double radarRange;
+
+  const Radar({
+    Key? key,
+    required this.radarRange,
+  }) : super(key: key);
+
+  @override
+  State<Radar> createState() => _RadarState();
+}
+
+class _RadarState extends State<Radar> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 7),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0, end: pi * 2).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.repeat();
+      }
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        AnimatedBuilder(
+          animation: _animation,
+          builder: (context, _) {
+            return Stack(
+              children: [
+                Transform.rotate(
+                  angle: _animation.value - (pi / 2),
+                  child: CustomPaint(
+                    size: size,
+                    painter: RadarBackgroundPainter(
+                      radarRange: widget.radarRange,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+        CustomPaint(
+          size: size,
+          painter: RadarLinePainter(),
+        ),
+      ],
+    );
+  }
+}
